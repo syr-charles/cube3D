352e3772b7f8bd230dd6fe71af289af05012dfa7
Charles Dana <charles@macbook-pro-de-charles.home>
2020-07-18 09:53:23 +0200

    fix sprites

--- ft_draw_sprites.c
+++ ft_draw_sprites.c
@@ -6,7 +6,7 @@
 /*   By: cdana <cdana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 19:36:07 by cdana             #+#    #+#             */
-/*   Updated: 2020/07/14 07:41:09 by charles          ###   ########.fr       */
+/*   Updated: 2020/07/18 09:50:35 by charles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ static int		ft_draw_sprite_col(t_mlx *f, int col, int i)
 	dx = atan(tan(f->sp_an[f->od[i]] - f->alpha) - 
 			tan(f->wall_angle[col] - f->alpha)) * lbd;
 	if (fabs(ft_circle(f->sp_an[f->od[i]] - f->alpha)) < M_PI / 2 - 0.2 &&
-		lbd > 0.314 && (dx = 0.5 + dx * f->sp_dt[f->od[i]]) >= 0 && dx <= 1)
+		lbd > 0.45 && (dx = 0.5 + dx * f->sp_dt[f->od[i]]) >= 0 && dx <= 1)
 	{
 		y = 0;
 		while (y < f->res_y)
